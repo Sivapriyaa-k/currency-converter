ada821b5bcacfa20aba35e231d1210def3a2e793
Sivapriyaa-k <sivapriyakalamegam@gmail.com>
2026-02-16 21:19:11 +0530

    updated  App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import useCurrencyInfo from "./hooks/useCurrencyInfo";
 
 function App() {
   const [amount, setAmount] = useState(0);
   const [from, setFrom] = useState("usd");
   const [set, setTo] = useState("inr");
   const [convertedAmount, setConvertedAmount] = useState(0);
+
+  const currencyInfo = useCurrencyInfo(from);
   return (
     <div
       className="w-full h-screen flex flex-wrap 
