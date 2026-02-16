c4224208fe5b4f0a3ddc059d0f2c5a1e74979a71
Sivapriyaa-k <sivapriyakalamegam@gmail.com>
2026-02-16 20:09:50 +0530

    updated  app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import "./App.css";
 function App() {
   const [amount, setAmount] = useState(0);
   const [from, setFrom] = useState("usd");
-
+  const [set, setTo] = useState("inr");
+  const [convertedAmount, setConvertedAmount] = useState(0);
   return (
     <div
       className="w-full h-screen flex flex-wrap 
