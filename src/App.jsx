df99ddb30aff56b9af2959f64df14d7536b39bce
Sivapriyaa-k <sivapriyakalamegam@gmail.com>
2026-03-02 21:44:34 +0530

    updated  App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,11 @@ import { InputBox } from "./components/index.js";
 function App() {
   const [amount, setAmount] = useState(0);
   const [from, setFrom] = useState("usd");
-  const [set, setTo] = useState("inr");
+  const [to, setTo] = useState("inr");
   const [convertedAmount, setConvertedAmount] = useState(0);
 
   const currencyInfo = useCurrencyInfo(from);
-  const options = objects.keys(currencyInfo);
+  const options = Object.keys(currencyInfo);
 
   const swap = () => {
     setFrom(to);
@@ -26,7 +26,7 @@ function App() {
   return (
     <div
       className="w-full h-screen flex flex-wrap 
-    justify-center items-center bg-cover bg=no-repeat"
+    justify-center items-center bg-cover bg-no-repeat"
       style={{
         backgroundImage: `url(https://images.pexels.com/photos/4968651/pexels-photo-4968651.jpeg)`,
       }}
@@ -45,6 +45,24 @@ function App() {
                 amount={amount}
                 currencyOptions={options}
                 onCurrencyChange={(currency) => setFrom(currency)}
+                onAmountChange={(amount) => setAmount(amount)}
+              />
+            </div>
+          </form>
+        </div>
+        <div className="w-full max-w-md mx-auto my-10 border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              convert();
+            }}
+          >
+            <div className="w-full mb-1">
+              <InputBox
+                label="to"
+                amount={amount}
+                currencyOptions={options}
+                onCurrencyChange={(currency) => setTo(currency)}
                 onAmountChange={(amount) => setAmount(amount)}
               />
             </div>
